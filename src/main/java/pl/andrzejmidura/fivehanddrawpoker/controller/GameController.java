9d512edbeec24dcacaab71048150e18f82d99d2d
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-26 09:53:44 +0200

    added /newRound endpoint for receiving MessageToSinglePlayer at the beginning of every round, deleted /action

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/controller/GameController.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/controller/GameController.java
@@ -1,54 +1,30 @@
 package pl.andrzejmidura.fivehanddrawpoker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestContextHolder;
-import pl.andrzejmidura.fivehanddrawpoker.entity.User;
-import pl.andrzejmidura.fivehanddrawpoker.game.response.GameMessage;
-import pl.andrzejmidura.fivehanddrawpoker.game.response.PlayerState;
-import pl.andrzejmidura.fivehanddrawpoker.game.utils.*;
+import pl.andrzejmidura.fivehanddrawpoker.game.messageDTO.MessageToSinglePlayer;
+import pl.andrzejmidura.fivehanddrawpoker.service.GameService;
 
-import java.util.List;
-import java.util.Map;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/game")
 public class GameController {
 
+    @Autowired
+    GameService gameService;
+
     @GetMapping("/")
-    public String index(Model model) {
-
-        // TESTING UI
-        GameMessage gameMessage = new GameMessage();
-        gameMessage.setResponseType(PlayerState.WAIT);
-        gameMessage.setAvailableActions(List.of(Action.BET, Action.FOLD));
-        gameMessage.setCards(List.of(  new Card(Suit.HEART,    Rank.ACE),
-                                        new Card(Suit.SPADE,    Rank.ACE),
-                                        new Card(Suit.DIAMOND,  Rank.NINE),
-                                        new Card(Suit.DIAMOND,  Rank.JACK),
-                                        new Card(Suit.CLUB,     Rank.THREE)));
-        gameMessage.setCredits(200L);
-        gameMessage.setHand(Hand.PAIR);
-        gameMessage.setPot(0L);
-        User user1 = new User();
-        user1.setUsername("Andrzej");
-        User user2 = new User();
-        user2.setUsername("Maciek");
-        Map<User, Action> map = Map.of( user1, Action.NO_ACTION,
-                                        user2, Action.NO_ACTION);
-        gameMessage.setOtherPlayers(map);
-
-        model.addAttribute("gameResponse", gameMessage);
-        // END
-
+    public String index() {
         return "game";
     }
 
-    @GetMapping("/action")
-    public String action(@RequestParam(name = "name") String action) {
-        System.out.println("Session id[" + RequestContextHolder.currentRequestAttributes().getSessionId() + "]: " + action);
-        return "redirect:/";
+    @GetMapping("/newRound")
+    @ResponseBody
+    public MessageToSinglePlayer newRound(Principal principal) {
+        String username = principal.getName();
+        return gameService.messageToSinglePlayer(username);
     }
 
 }
