45b703386a3e627c48fc73b043233f51c011e9de
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-27 11:45:59 +0200

    init template for PlayerQueue

---
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/PlayerQueue.java
@@ -0,0 +1,7 @@
+package pl.andrzejmidura.fivehanddrawpoker.game;
+
+import java.util.LinkedList;
+
+public class PlayerQueue extends LinkedList<String> {
+    public void moveFirstPlayerAtTheEnd() {}
+}
