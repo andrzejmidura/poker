35ffa916cfff90283858a3cbe2bd0a4dd14de29e
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-27 11:50:37 +0200

    moved from /templates to /static

--- src/main/resources/templates/script.js
+++
@@ -1,7 +0,0 @@
-function changeAmount(val) {
-    var element = document.getElementById("amount");
-    var amount = parseInt(element.innerText);
-    if (amount+val>=1) amount += val;
-    else amount = 1;
-    element.innerText = amount;
-}
