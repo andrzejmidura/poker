ed06421e9dd0b836806f08b6c7993df4c0d226a3
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-28 19:08:18 +0200

    refactoring: changed to interface, added queueing methods signature

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/Game.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/Game.java
@@ -1,8 +1,12 @@
 package pl.andrzejmidura.fivehanddrawpoker.game;
 
-import pl.andrzejmidura.fivehanddrawpoker.game.utils.Deck;
+import pl.andrzejmidura.fivehanddrawpoker.game.messageDTO.MessageFromPlayer;
+import pl.andrzejmidura.fivehanddrawpoker.game.messageDTO.MessageToAllSubscribers;
+import pl.andrzejmidura.fivehanddrawpoker.game.messageDTO.MessageToSinglePlayer;
 
-public class Game {
-    private final Deck deck = new Deck();
-
+public interface Game {
+    MessageToSinglePlayer getMessageToSinglePlayer(String username);
+    MessageToAllSubscribers getMessageToAllSubscribers(String username, MessageFromPlayer message);
+    void queueNewPlayer(String username);
+    void unqueuePlayer(String username);
 }
