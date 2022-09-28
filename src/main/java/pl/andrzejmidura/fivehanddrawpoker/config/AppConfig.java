928df6d5e87c2c540d0804f45fa79ba498641407
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-28 19:06:18 +0200

    added Game bean

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/config/AppConfig.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/config/AppConfig.java
@@ -1,7 +1,14 @@
 package pl.andrzejmidura.fivehanddrawpoker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.andrzejmidura.fivehanddrawpoker.game.Game;
+import pl.andrzejmidura.fivehanddrawpoker.game.GameImpl;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    public Game game() {
+        return new GameImpl();
+    }
 }
