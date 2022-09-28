241fc2f80070c8e481968f3fb2c2c6f15256d8ae
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-28 19:05:59 +0200

    added more actions in order to use in different game stages

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/utils/Action.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/utils/Action.java
@@ -1,11 +1,18 @@
 package pl.andrzejmidura.fivehanddrawpoker.game.utils;
 
 public enum Action {
-        NO_ACTION,
+        // action reserved for ANTE stage
+        ANTE,
+
+        // actions reserved for BETTING stage
         BET,
         CHECK,
         FOLD,
         CALL,
         RAISE,
-        ALLIN
+        ALL_IN,
+
+        // actions reserved for DISCARDING stage
+        DISCARD,
+        KEEP_ALL
 }
