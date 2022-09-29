cea78997c825bcadb5db5e3eae74e90dbed351b7
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-29 10:22:51 +0200

    changed evaluate(Card[]) into evaluate(List<Card>)

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/HandEvaluator.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/HandEvaluator.java
@@ -3,8 +3,10 @@ package pl.andrzejmidura.fivehanddrawpoker.game;
 import pl.andrzejmidura.fivehanddrawpoker.game.utils.Card;
 import pl.andrzejmidura.fivehanddrawpoker.game.utils.Hand;
 
+import java.util.List;
+
 public class HandEvaluator {
-    public static Hand evaluate(Card[] cards) {
+    public static Hand evaluate(List<Card> cards) {
         return null;
     }
 }
