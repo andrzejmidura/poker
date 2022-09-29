3439bbe912027dda6129d7b44f2fe09821a929ae
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-29 10:23:42 +0200

    added dynamic greeting at homepage

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/controller/HomeController.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/controller/HomeController.java
@@ -1,13 +1,17 @@
 package pl.andrzejmidura.fivehanddrawpoker.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String homepage() {
+    public String homepage(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("username", username);
         return "homepage";
     }
 }
