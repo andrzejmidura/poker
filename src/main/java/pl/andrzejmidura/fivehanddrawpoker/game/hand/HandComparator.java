ca9dbf3f4945af18e29135c509360ca4177167ac
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-30 11:20:39 +0200

    init

---
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/hand/HandComparator.java
@@ -0,0 +1,12 @@
+package pl.andrzejmidura.fivehanddrawpoker.game.hand;
+
+import pl.andrzejmidura.fivehanddrawpoker.game.utils.Card;
+
+import java.util.Comparator;
+
+public class HandComparator implements Comparator<HandWithCards> {
+    @Override
+    public int compare(HandWithCards o1, HandWithCards o2) {
+        return 0;
+    }
+}
