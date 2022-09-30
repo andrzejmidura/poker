b3f46ac48d7c0f9f3241e1d05084b8259c7b36df
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-09-30 11:29:13 +0200

    added equals() and hashcode()

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/utils/Card.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/utils/Card.java
@@ -1,5 +1,12 @@
 package pl.andrzejmidura.fivehanddrawpoker.game.utils;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
 public class Card {
     private Suit suit;
     private Rank rank;
@@ -11,27 +18,24 @@ public class Card {
         this.rank = rank;
     }
 
-    public Suit getSuit() {
-        return suit;
-    }
-
-    public void setSuit(Suit suit) {
-        this.suit = suit;
-    }
-
-    public Rank getRank() {
-        return rank;
-    }
-
-    public void setRank(Rank rank) {
-        this.rank = rank;
-    }
-
     @Override
     public String toString() {
         return "Card{" +
                 "suit=" + suit +
                 ", rank=" + rank +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Card)) return false;
+        Card card = (Card) o;
+        return suit == card.suit && rank == card.rank;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit, rank);
     }
 }
