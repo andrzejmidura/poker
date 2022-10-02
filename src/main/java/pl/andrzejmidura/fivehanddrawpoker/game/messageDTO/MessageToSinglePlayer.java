21c67a50d2a217f5646737569d3119ab48978d3b
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-10-02 17:36:43 +0200

    deleted redundant imports

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/messageDTO/MessageToSinglePlayer.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/messageDTO/MessageToSinglePlayer.java
@@ -2,13 +2,11 @@ package pl.andrzejmidura.fivehanddrawpoker.game.messageDTO;
 
 import lombok.Getter;
 import lombok.Setter;
-import pl.andrzejmidura.fivehanddrawpoker.entity.User;
 import pl.andrzejmidura.fivehanddrawpoker.game.utils.Action;
 import pl.andrzejmidura.fivehanddrawpoker.game.utils.Card;
-import pl.andrzejmidura.fivehanddrawpoker.game.utils.Hand;
+import pl.andrzejmidura.fivehanddrawpoker.game.hand.Hand;
 
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @Setter
