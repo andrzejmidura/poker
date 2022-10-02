5d41abb4e90208c2c9ffa096abd8b47cee937f62
amidura@student.agh.edu.pl <amidura@student.agh.edu.pl>
2022-10-02 17:37:19 +0200

    changed to subclass of RuntimeException

--- src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/hand/InvalidNumberOfCardsToEvaluateException.java
+++ src/main/java/pl/andrzejmidura/fivehanddrawpoker/game/hand/InvalidNumberOfCardsToEvaluateException.java
@@ -1,6 +1,6 @@
 package pl.andrzejmidura.fivehanddrawpoker.game.hand;
 
-public class InvalidNumberOfCardsToEvaluateException extends Exception {
+public class InvalidNumberOfCardsToEvaluateException extends RuntimeException {
     public InvalidNumberOfCardsToEvaluateException(String message) {
         super(message);
     }
